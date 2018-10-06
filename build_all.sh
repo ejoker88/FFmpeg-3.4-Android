692f703605d8dea3cd9c7685a8a0b33f02892c57
Rajdeep Rajput <30290459+ejoker88@users.noreply.github.com>
2018-10-06 18:31:09 +0200

    Permission denied issue
    Permission denied issue occurred for individual bash files, when build_all.sh script is run. To add permission to run individual (each architecture bash file) bash files, "chmod +x" command added.

--- build_all.sh
+++ build_all.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+chmod +x build_armeabi.sh
 ./build_armeabi.sh
+
+chmod +x build_armeabi-v7a.sh
 ./build_armeabi-v7a.sh
+
+chmod +x build_arm64-v8a.sh
 ./build_arm64-v8a.sh
+
+chmod +x build_x86.sh
 ./build_x86.sh
+
+chmod +x build_x86_64.sh
 ./build_x86_64.sh
